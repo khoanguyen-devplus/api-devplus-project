31ae2e648ed78c67c6187066724ba21d0afa4c24
Khoa Nguyen <khoanguyen1962001@gmail.com>
2022-08-22 11:12:00 +0700

    create concerns api

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,3 +1,4 @@
 module.exports.sidebarController = require("./sidebar.controller");
 module.exports.bannerController = require("./banner.controller");
 module.exports.aboutController = require("./about.controller");
+module.exports.concernController = require("./concern.controller");
--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,4 @@
 module.exports.sidebarService = require("./sidebar.service");
 module.exports.bannerService = require("./banner.service");
 module.exports.aboutService = require("./about.service");
+module.exports.concernService = require("./concern.service");
