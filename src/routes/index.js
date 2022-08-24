fb901b7af472bcb1b98411654d42cd301de648e9
nhutnguyen2000 <nhut.nguyen@devplus.edu.vn>
2022-08-24 10:06:03 +0700

    fix config cors

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,6 +11,11 @@ const admissionRoute = require("./admission.route");
 
 const router = express.Router();
 
+router.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 router.use('/sidebar', sidebarRoute);
 router.use('/banner', bannerRoute);
 router.use('/about', aboutRoute);
